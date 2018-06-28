55ebb47b05eb2f3ee408956adaf1b6a29a07ea50
Ahmad <ahmad.hussain.307@gmail.com>
2018-06-29 00:24:27 +0200

    Added new connection options

--- mqtt_showcase/mqtt/paho_client.py
+++ mqtt_showcase/mqtt/paho_client.py
@@ -2,10 +2,16 @@ import paho.mqtt.client as paho
 import time
 
 class Connection:
-    def __init__(self):
-        self.client = paho.Client()
+    """Establishes MQTT connection through Paho python client"""
+    def __init__(self,id,username=None,password=None):
+        """Creates a client instance with a random ID if no ID is given"""
+        self.client = paho.Client(client_id=id)
         self.client.on_connect = self.on_connect
         self.client.on_log = self.on_log
+        self.client_id = self.client._client_id
+
+        if username!=None and password!=None:
+            self.client.username_pw_set(username,password)
 
     def on_connect(self,client, userdata, flags, rc):
         # Debug message
@@ -15,7 +21,8 @@ class Connection:
         # Debug message
         print('log!')
 
-    def connect(self,broker,port):
-        self.client.connect(broker, port)
+    def connect(self,broker,port,keepalive):
+        """Establishes a connection with the given host"""
+        self.client.connect(host=broker, port=port,keepalive=keepalive)
         self.client.loop_start()
         self.client.loop_stop()
--- mqtt_showcase/mqtt/views.py
+++ mqtt_showcase/mqtt/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render
 from mqtt.paho_client import Connection
 
 def index(request):
-    result = {}
+    response = {}
+    response['submit_btn_text'] = 'Connect'
     if request.method == 'POST':
-        client = Connection()
+        # Get entered data
         broker = request.POST.get('host')
         port = int(request.POST.get('port'))
+        id = request.POST.get('client_id')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        keepalive = int(request.POST.get('keep_alive'))
 
-        client.connect(broker,port)
-        result['connection_result'] = 'Connected'
+        # Establish a connection through 'Connection' class
+        if len(username)!=0 and len(password)!=0:
+            client = Connection(id,username,password)
+        else:
+            client = Connection(id)
+        client.connect(broker,port,keepalive)
 
-    return render(request, 'mqtt/index.html', result)
+        response['connection_result'] = 'Connected'
+        response['submit_btn_text'] = 'Disconnect'
+
+    return render(request, 'mqtt/index.html', response)
